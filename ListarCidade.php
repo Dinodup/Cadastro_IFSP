1bdc9f26c513691056242af1cbcf9f0435b9f85f
Dinodup <dudup2626@gmail.com>
2024-07-02 12:19:41 -0300

    comita

--- ListarCidade.php
+++ ListarCidade.php
@@ -4,39 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Consulta de Cidades</title>
+    <link rel="stylesheet" href="style1.css">
 </head>
 <body>
-<?php
-    include('includes/conexao.php');
-    $sql = "SELECT * FROM cidade";
+    <?php
+        include('includes/conexao.php');
+        $sql = "SELECT * FROM cidade";
 
-    //Executa a consulta
-    $result = mysqli_query($con,$sql);
+        // Executa a consulta
+        $result = mysqli_query($con, $sql);
+    ?>
 
-    //Retorna apenas uma linha
-    $row = mysqli_fetch_array($result);
-?>
+    <div class="container">
+        <h1>Consulta de Cidades</h1>
+        <a href="CadastroCidade.html">Cadastrar nova Cidade</a><br>
+        <a href="inicial.html">Voltar para a Tela Inicial</a>
 
-    <h1>Consulta de Cidades</h1>
-    <a href="CadastroCidade.html">Cadastrar nova Cidade<br></a>
-    <a href="inicial.html">Voltar para a Tela Inicial</a>
-
-    <table align="center" border="1" width="500">
-        <tr>
-            <th>Código</th>
-            <th>Cidade</th>
-            <th>Estado</th>
-        </tr>
-        <?php
-            while($row = mysqli_fetch_array($result)){
-                echo "<tr>";
-                echo "<td>".$row['id']."</td>";
-                echo "<td>".$row['nome']."</td>";
-                echo "<td>".$row['estado']."</td>";
-                echo "</tr>";
-            }
-        ?>
-    </table>
+        <table class="city-table">
+            <tr>
+                <th>Código</th>
+                <th>Cidade</th>
+                <th>Estado</th>
+            </tr>
+            <?php
+                while($row = mysqli_fetch_array($result)){
+                    echo "<tr>";
+                    echo "<td>".$row['id']."</td>";
+                    echo "<td>".$row['nome']."</td>";
+                    echo "<td>".$row['estado']."</td>";
+                    echo "</tr>";
+                }
+            ?>
+        </table>
+    </div>
 </body>
 </html>
