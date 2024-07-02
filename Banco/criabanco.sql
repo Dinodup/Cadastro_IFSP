48e09f3ab3fe96b3cebf258e5a5e763f36550f12
Dinodup <dudup2626@gmail.com>
2024-07-02 10:31:38 -0300

    comita

--- Banco/criabanco.sql
+++ Banco/criabanco.sql
@@ -19,12 +19,12 @@ CREATE TABLE Cidade
 -- Cria tabela de clientes.
 CREATE TABLE Cliente
 (
-id          INT AUTO INCREMENT,
-nome        VARCHAR(100),
-email       VARCHAR(100),
-senha       VARCHAR(50),
-ativo       BOOL,
-id_cidade   INT,
-PRIMARY KEY(id),
-CONSTRAINT FK_ClienteCidade FOREIGN KEY (id_cidade) REFERENCES Cidade(id)
+    id          INT AUTO INCREMENT,
+    nome        VARCHAR(100),
+    email       VARCHAR(100),
+    senha       VARCHAR(50),
+    ativo       BOOL,
+    id_cidade   INT,
+    PRIMARY KEY(id),
+    CONSTRAINT FK_ClienteCidade FOREIGN KEY (id_cidade) REFERENCES Cidade(id)
 );
